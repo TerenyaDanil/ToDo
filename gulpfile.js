d0fc7d8f8e02b7f89bd59fbebc10ccf7eb85fc4b
Данил <apple@MacBook-Pro-apple.local>
2024-05-16 22:51:04 +0500

    Add animation, add watch on js for tailwind

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,10 @@
-const gulp = require('gulp');
+const gulp = require("gulp");
 
-require('./gulp/dev.js');
-require('./gulp/docs.js');
+require("./gulp/dev.js");
+require("./gulp/docs.js");
 
 // Запуск дефолтного таска gulp
-gulp.task('default', gulp.series(
-	'clean:dev',
-	gulp.parallel('includeFiles:dev', 'tailwind:dev', 'copyImages:dev', 'copyFonts:dev', 'copyFiles:dev', 'js:dev'),
-	gulp.parallel('startServer:dev', 'watch:dev')
-));
+gulp.task("default", gulp.series("clean:dev", gulp.parallel("includeFiles:dev", "tailwind:dev", "copyImages:dev", "copyFonts:dev", "copyFiles:dev", "js:dev"), gulp.parallel("startServer:dev", "watch:dev")));
 
 // Запуск продакшн таска gulp
-gulp.task('docs', gulp.series(
-	'clean:docs',
-	gulp.parallel('includeFiles:docs', 'tailwind:docs', 'copyImages:docs', 'copyFonts:docs', 'copyFiles:docs', 'js:docs'),
-	gulp.parallel('startServer:docs')
-));
+gulp.task("docs", gulp.series("clean:docs", gulp.parallel("includeFiles:docs", "tailwind:docs", "copyImages:docs", "copyFonts:docs", "copyFiles:docs", "js:docs"), gulp.parallel("startServer:docs")));
